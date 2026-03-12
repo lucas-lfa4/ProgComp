b95ef7b69fef50c64bb88302b3f125cf114c9f12
Lucas Alves <150536112+lucas-lfa4@users.noreply.github.com>
2026-03-12 11:00:13 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); //desincronizar as bibliotecas do c e c++
+    cin.tie(nullptr); //desincroniza os buffers de input e output
+
+    return 0;
+}
