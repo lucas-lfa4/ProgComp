f8a6f61e9000e88fe0377bdc2bc9de79506fbace
lucas-lfa4 <lfa4@cin.ufpe.br>
2026-04-14 19:21:40 -0300

    a

--- Lista4/B.cpp
+++ Lista4/B.cpp
@@ -6,6 +6,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
+    int t, n, x;
+    cin >> t;
+    vector<int> a;
+
+    for(int i = 0; i < t; i++) {
+        cin >> n >> x;
+        a.resize(n);
+
+        for(int j = 0; j < n; j++) {
+            cin >> a[j];
+        }
+    }
 
 
     return 0;
