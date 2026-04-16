3279b985358a0eeb045133c7880ba562bf700730
lucas-lfa4 <lfa4@cin.ufpe.br>
2026-04-16 09:31:54 -0300

    B EP4

--- EP4/B.cpp
+++ EP4/B.cpp
@@ -2,43 +2,77 @@
 
 using namespace std;
 
+bool isSafe(char chess[8][8], int linha, int coluna);
+void placeQueens(int coluna, char chess[8][8], int& resultado);
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     char chess[8][8];
-    bool colunas[8];
 
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++) {
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
             cin >> chess[i][j];
-        }
-        colunas[i] = true;
-    }
 
-    for(int a = 0; a < 8; a++) {
-        if(colunas[a] && chess[0][a] == '.') {
-            
-        }
-        for(int b = 0; b < 8; b++) {
-            for(int c = 0; c < 8; c++) {
-                for(int d = 0; d < 8; d++) {
-                    for(int e = 0; e < 8; e++) {
-                        for(int f = 0; f < 8; f++) {
-                            for(int g = 0; g < 8; g++) {
-                                for(int h = 0; h < 8; h++) {
-                                    
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    int resultado = 0;
 
-    
+    placeQueens(0, chess, resultado);
 
+    cout << resultado;
 
     return 0;
+}
+
+bool isSafe(char chess[8][8], int linha, int coluna) {
+    if(chess[linha][coluna] == '*')
+        return false;
+    int i, j;
+    
+    //checar se existe uma rainha na mesma coluna
+    for(i = 0; i < linha; i++)
+        if(chess[i][coluna] == 'x')
+            return false;
+
+    //checar se existe uma rainha na diagonal
+    //superior direita
+    i = linha;
+    j = coluna;
+    while(i >= 0 && j <= 7) {
+        if(chess[i][j] == 'x')
+            return false;
+        else {
+            i--;
+            j++;
+        }
+    }
+    //superior esquerda
+    i = linha;
+    j = coluna;
+    while(i >=0 && j >= 0) {
+        if(chess[i][j] == 'x')
+            return false;
+        else {
+            i--;
+            j--;
+        }
+    }
+
+    return true;
+}
+
+void placeQueens(int linha, char chess[8][8], int& resultado) {
+    if(linha == 8) {
+        resultado++;
+        return;
+    }
+
+    for(int i = 0; i < 8; i++) {
+        if(isSafe(chess, linha, i)) {
+            chess[linha][i] = 'x';
+            placeQueens(linha+1, chess, resultado);
+
+            chess[linha][i] = '.';
+        }
+    }
 }
