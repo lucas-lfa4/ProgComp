7618c2d8ed83195e086027fce115bf6214424bce
lucas-lfa4 <lfa4@cin.ufpe.br>
2026-04-20 22:51:35 -0300

    B lista4

--- Lista4/C.cpp
+++ Lista4/C.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool possoColocarEsseNumero(int numero, int sudoku[9][9], int linha, int coluna);
-void colocarNumero(int sudoku[9][9], int linha, int coluna, int** resultado);
+void colocarNumero(int sudoku[9][9], int linha, int coluna, bool* existeSol);
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -11,23 +11,23 @@ int main() {
     
     int n;
     cin >> n;
-    char sudoku[9][9];
+    int sudoku[9][9];
+    bool existeSol = false;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) {
         for(int j = 0; j < 9; j++)
             for(int k = 0; k < 9; k++)
                 cin >> sudoku[j][k];
-    
-    
-    
+        colocarNumero(sudoku, 0, 0, &existeSol);
+        if(!existeSol)
+            cout << "No solution\n";
+        existeSol = false;
+    }
 
     return 0;
 }
 
 bool possoColocarEsseNumero(int numero, int sudoku[9][9], int linha, int coluna) {
-    if(sudoku[linha][coluna] != 0)
-        return false;
-    
     //checar se existe o mesmo numero na linha ou coluna
     for(int i = 0; i < 9; i++) {
         if(sudoku[linha][i] == numero)
@@ -37,66 +37,43 @@ bool possoColocarEsseNumero(int numero, int sudoku[9][9], int linha, int coluna)
     }
 
     //checar se existe o mesmo numero no mesmo quadrante
-    if(linha <= 2 && coluna <= 2) {
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha >= 3 && linha <= 5 && coluna <= 2) {
-        for(int i = 3; i < 6; i++)
-            for(int j = 0; j < 3; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha >= 6 && linha <= 8 && coluna <= 2) {
-        for(int i = 6; i < 9; i++)
-            for(int j = 0; j < 3; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha <= 2 && coluna >=3 && coluna <= 5) {
-        for(int i = 0; i < 3; i++)
-            for(int j = 3; j < 6; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha >= 3 && linha <= 5 && coluna >= 3 && coluna <= 5) {
-        for(int i = 3; i < 6; i++)
-            for(int j = 3; j < 6; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha >= 6 && linha <= 8 && coluna >= 3 && coluna <= 5) {
-        for(int i = 6; i < 9; i++)
-            for(int j = 3; j < 6; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha <= 2 && coluna >= 6 && coluna <= 8) {
-        for(int i = 0; i < 3; i++)
-            for(int j = 6; j < 9; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if(linha >= 3 && linha <= 5 && coluna >= 6 && coluna <= 8) {
-        for(int i = 3; i < 6; i++)
-            for(int j = 6; j < 9; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
-    else if (linha >= 6 && linha <= 8 && coluna >= 6 && coluna <= 8) {
-        for(int i = 6; i < 9; i++)
-            for(int j = 6; j < 9; j++)
-                if(sudoku[i][j] == numero)
-                    return false;
-    }
+    int comecoLinha = linha - (linha % 3), comecoColuna = coluna - (coluna % 3);
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (sudoku[i + comecoLinha][j + comecoColuna] == numero)
+                return false;
+
+    return true;
 }
 
-void colocarNumero(int sudoku[9][9], int linha, int coluna, int** resultado) {
-    if(linha == 9) {
+void colocarNumero(int sudoku[9][9], int linha, int coluna, bool* existeSol) {
+    if(linha == 9 && coluna == 8) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++)
+                cout << sudoku[i][j] << " ";
 
+            cout << "\n";
+        }
+        *existeSol = true;
+        return;
     }
 
-    
+    if(linha == 9) {
+        coluna++;
+        linha = 0;
+    }
+
+    if(sudoku[linha][coluna] != 0) {
+        colocarNumero(sudoku, linha+1, coluna, existeSol);
+        return;
+    }
+
+    for(int i = 1; i <= 9; i++) {
+        if(possoColocarEsseNumero(i, sudoku, linha, coluna)) {
+            sudoku[linha][coluna] = i;
+            colocarNumero(sudoku, linha+1, coluna, existeSol);
+            sudoku[linha][coluna] = 0;
+        }
+    }
 }
