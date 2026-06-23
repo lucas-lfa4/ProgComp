3fb9cc14c38c3007acd46f8fb75f037ad7338dc4
lucas-lfa4 <lfa4@cin.ufpe.br>
2026-06-22 21:55:39 -0300

    Questão B lista 2

---
+++ Lista2/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, n, soma = 0;
+    cin >> t;
+
+    for(int i = 0; i < t; i++, soma = 0) {
+        cin >> n;
+        vector<int> s(n);
+        priority_queue<int> pq;
+        for(int j = 0; j < n; j++) {
+            cin >> s[j];
+            pq.emplace(s[j]);
+            if(s[j] == 0) {
+                soma += pq.top();
+                pq.pop(); 
+            }
+        }
+
+        cout << soma << "\n";
+    }
+
+    return 0;
+}
